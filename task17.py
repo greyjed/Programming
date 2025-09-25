762feeccf3dbcf7b77bb6a8ed032f81a7ea1b3a2
greyjed <greyjedi275@gmail.com>
2025-09-25 19:01:24 +0300

    Update task17.py

--- task17.py
+++ task17.py
@@ -1,8 +1,3 @@
-while True:
-    try:
-        a = input("Введіть символ (або натисніть Enter для завершення): ")
-        if a == "":
-            break
-        print(ord(a))
-    except Exception as e:
-        print("Помилка:", e)
+a = input()
+
+print(ord(a))
